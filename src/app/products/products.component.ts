da56593ddf581257179fe2fba9ab6b83bf4d72f8
carbonsoftware <dipon.thc@hotmail.co.uk>
2017-06-07 03:13:59 +0100

    changes to products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,18 +7,21 @@ import {product} from "./product.model";
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products: product[] = [];
-  test1: product = new product('name', 'description of the product', 503, '../');
-  test2: product = new product('second', 'description of the product', 534);
-  test3: product = new product('third', 'description of the product', 324);
-  test4: product = new product('fourth', 'description of the product', 352);
+  products: product[] = [
+    new product('name', 'description of the product', 503, '../'),
+    new product('second', 'description of the product', 534),
+    new product('third', 'description of the product', 324),
+    new product('fourth', 'description of the product', 352),
+    new product('fifth', 'description of the product', 503, '../'),
+    new product('sixth', 'description of the product', 534),
+    new product('third', 'description of the product', 324),
+    new product('fourth', 'description of the product', 352)
+  ];
+
 
   constructor() {
     //service will get products from database and inject them here
-    this.products.push(this.test1);
-    this.products.push(this.test2);
-    this.products.push(this.test3);
-    this.products.push(this.test4);
+
   }
 
   ngOnInit() {
