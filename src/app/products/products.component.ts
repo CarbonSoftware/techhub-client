670497dcda3435f1bf6e2fad50d7640d95f99df1
carbonsoftware <dipon.thc@hotmail.co.uk>
2017-06-08 15:07:12 +0100

    sort issue solved

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -11,22 +11,24 @@ export class ProductsComponent implements OnInit, DoCheck {
   products: product[] = [];
   sortType: string = 'relevance';
 
-  constructor(private prodcutService: ProductsService) {
+  constructor(private productService: ProductsService) {
     //service will get products from database and inject them here
-    this.products = prodcutService.getProducts();
+    this.products = productService.getProducts();
 
   }
 
   sort() {
     console.log(this.sortType);
     if (this.sortType === 'relevance') {
-
+      this.products.sort();
     }else if (this.sortType === 'priceAscending') {
       this.products.sort(function(a, b){
-        return a.price - b.price;
-      })
+        return a.price - b.price
+      });
     }else if (this.sortType === 'priceDescending') {
-
+      this.products.sort(function(a, b) {
+        return b.price - a.price
+      });
     } else {
       console.log('error: ' + this.sortType + ' is not recognised');
     }
@@ -37,7 +39,6 @@ export class ProductsComponent implements OnInit, DoCheck {
   }
 
   ngDoCheck() {
-    this.sort();
   }
 
 }
