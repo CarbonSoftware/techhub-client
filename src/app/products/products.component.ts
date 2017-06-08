7d890da9f3a55ce7e7e87429ecd92329ac6424b2
carbonsoftware <dipon.thc@hotmail.co.uk>
2017-06-08 03:30:34 +0100

    updated products-components and added product sort feature

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {product} from "./product.model";
+import {forEach} from "@angular/router/src/utils/collection";
 
 @Component({
   selector: 'app-products',
@@ -13,15 +14,37 @@ export class ProductsComponent implements OnInit {
     new product('third', 'description of the product', 324),
     new product('fourth', 'description of the product', 352),
     new product('fifth', 'description of the product', 503, '../'),
-    new product('sixth', 'description of the product', 534),
-    new product('third', 'description of the product', 324),
-    new product('fourth', 'description of the product', 352)
+    new product('sixth', 'description of the product', 514),
+    new product('8th', 'description of the product', 324),
+    new product('9th', 'description of the product', 239),
+    new product('10th', 'description of the product', 399, '../'),
+    new product('11th', 'description of the product', 456),
+    new product('12th', 'description of the product', 300),
+    new product('13th', 'description of the product', 646),
+    new product('14th', 'description of the product', 700, '../'),
+    new product('15th', 'description of the product', 249),
+    new product('16th', 'description of the product', 150)
   ];
-
+  sortType: string = 'relevance';
 
   constructor() {
     //service will get products from database and inject them here
 
+  }
+
+  sort(sortType: string) {
+    if (sortType === 'relevance') {
+
+    }
+    if (sortType === 'priceAscending') {
+      this.products.sort(function(a, b){
+        return parseFloat(a.price.toString()) - parseFloat(b.price.toString());
+      })
+    }
+    if (sortType === 'priceDescending') {
+
+    }
+    console.log('error: ' + sortType + 'is not recognised')
   }
 
   ngOnInit() {
